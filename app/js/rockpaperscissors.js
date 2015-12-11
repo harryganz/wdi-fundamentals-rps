853ea5ef6801ebe2bfa00072bc40b2eb8a2913bb
Harry Ganz <hganz@rsmas.miami.edu>
2015-12-10 20:40:19 -0500

    completed game logic

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,7 +25,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move || getInput();
+    return move || getInput().replace(/\n$/, "");
 }
 
 function getComputerMove(move) {
@@ -57,6 +57,29 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while(playerWins < 5 && computerWins < 5){
+      // Initalize current move to null
+      var playerMove = null;
+      var computerMove = null;
+      // Initialize winner to null
+      var winner = null;
+      // Prompt player for move
+      playerMove = getPlayerMove();
+      console.log("Your move: " + playerMove);
+      // Computer move
+      computerMove = getComputerMove();
+      console.log("Computer's move: " + computerMove);
+      // See who won
+      winner = getWinner(playerMove, computerMove);
+      // Add wins to playerWins or computerWins
+      if(winner === 'computer'){
+        computerWins += 1;
+      }
+      if(winner === 'player') {
+        playerWins += 1;
+      } // Otherwise tie and re-run loop
+      console.log(winner + " wins this round. Player: " + playerWins +
+      " Computer: " + computerWins);
+    }
     return [playerWins, computerWins];
 }
